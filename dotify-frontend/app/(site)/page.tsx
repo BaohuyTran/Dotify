24efacee695bfa4e29a31be785d2c09a1041defd
BaohuyTran <baohuyt11@gmail.com>
2023-10-25 21:57:46 +0700

    main page - header - buttons

--- dotify-frontend/app/(site)/page.tsx
+++ dotify-frontend/app/(site)/page.tsx
@@ -1,5 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">Hello, This is Dotify!</div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
+      <Header>This is Header</Header>
+    </div>
   )
 }
